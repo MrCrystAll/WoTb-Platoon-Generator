76dd8c80fea689fc3d7d38f5d155312b04dbdc93
Mathieu Suchet <mathieu.suchet@ecole-hexagone.com>
2022-12-28 15:59:32 +0100

    Branch repaired

---
+++ WotGenC/Database/DbPlayer.cs
@@ -0,0 +1,42 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace WotGenC.Database
+{
+    public class DbPlayer
+    {
+        public Player Player;
+
+        public DbPlayer(Player player)
+        {
+            Player = player;
+        }
+
+        public bool WriteToDb()
+        {
+            SqlConnection cnn = new SqlConnection(Settings.ConnectionString);
+            cnn.Open();
+            
+            if (cnn.State != ConnectionState.Open) return false;
+
+            SqlCommand queryCheck = new SqlCommand($"SELECT * FROM Player WHERE ID = {Player.Id}", cnn);
+
+            var reader = queryCheck.ExecuteReader();
+            if (reader.HasRows)
+            {
+                reader.Close();
+                cnn.Close();
+                return false;
+            }
+            
+            reader.Close();
+            
+            SqlCommand command = new SqlCommand($"INSERT INTO Player VALUES ({int.Parse(Player.Id)},'{Player.Pseudo}','{Player.Token}')", cnn);
+
+            command.ExecuteNonQuery();
+            
+            cnn.Close();
+            return true;
+        }
+    }
+}
